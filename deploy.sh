472dfc6c101c6d6b5ecc6fd9e10f18f2819810d1
tesfish2012 <tesfay3814@gmail.com>
2024-02-21 09:16:51 -0900

    commit for GitHub Pages

--- deploy.sh
+++ deploy.sh
@@ -11,5 +11,5 @@ echo 'www.tesfit.com' > CNAME
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:tesfish2012/portfolio-.git main
+git push -f git@github.com:tesfish2012/portfolio-.git 
 cd -
