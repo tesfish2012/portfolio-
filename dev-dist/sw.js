08a4fe56ce789c673fba3ff57a57c5bd2bf81eeb
tesfish2012 <tesfay3814@gmail.com>
2024-02-21 09:01:43 -0900

    Initial commit for GitHub Pages

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9ohkrgs3sm"
+    "revision": "0.45qpr6nbr28"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
