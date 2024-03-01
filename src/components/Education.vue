7a63224b1b45f93a0e00b29c264b953c5ee07449
tesfish2012 <tesfay3814@email.com>
2024-02-29 23:39:28 -0900

    commit

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -1,55 +1,60 @@
 <template>
-    <section class="faq-section py-5" id="education">
-      <div class="container single-col-max">
-        <div class="row text-center">
-          <div class="col-12">
-            <div class="about-title">
-              <h3 class="text-uppercase title-text" style="font-size: 35px;">Education</h3>
-            </div>
-          </div>
-        </div>
-        <div id="faq-accordion" class="faq-accordion mx-auto">
-          <div class="card border-0 mb-4 shadow-sm">
-            <div class="card-header border-0 p-4 theme-bg-light">
-              <h4 class="card-title mb-0 exp">
-                <i class="fas fa-school mr-2"></i>
-                <a data-parent="#faq-accordion" data-toggle="collapse" class="card-toggle collapsed"
-                  href="#education1" aria-expanded="false">
-                  BSC Bachelor degree of science in Information technologies MEKELLE INSTITUTE OF TECHNOLOGY (MIT)
-                </a>
-                <span>2024</span>
-              </h4><br>
-              <h4 class="card-title mb-0 exp">
-                <i class="fas fa-school mr-2"></i>
-                <a data-parent="#faq-accordion" data-toggle="collapse" class="card-toggle collapsed"
-                  href="#education1" aria-expanded="false">
-                  BSC Bachelor degree of science in Information technologies MEKELLE INSTITUTE OF TECHNOLOGY (MIT)
-                </a>
-                <span>2024</span>
-              </h4><br>
-              <h4 class="card-title mb-0 exp">
-                <i class="fas fa-school mr-2"></i>
-                <a data-parent="#faq-accordion" data-toggle="collapse" class="card-toggle collapsed"
-                  href="#education1" aria-expanded="false">
-                  BSC Bachelor degree of science in Information technologies MEKELLE INSTITUTE OF TECHNOLOGY (MIT)
-                </a>
-                <span>2024</span>
-              </h4>
-            </div>
+  <section class="faq-section py-5" id="education">
+    <div class="container single-col-max">
+      <div class="row text-center">
+        <div class="col-12">
+          <div class="about-title">
+            <h3 class="text-uppercase title-text" style="font-size: 35px;">Education</h3>
           </div>
         </div>
       </div>
-    </section>
-  </template>
-  
-  <style scoped>
-  .mr-auto {
-    margin-right: auto !important;
-  }
-  </style>
-  
-  <script>
-  export default {
-    name: 'MyExperience'
-  }
-  </script>
+      <div id="faq-accordion" class="faq-accordion mx-auto">
+        <div class="card border-0 mb-4 shadow-sm">
+          <div class="card-header border-0 p-4 theme-bg-light">
+            <h4 class="card-title mb-0 exp">
+              <i class="fas fa-graduation-cap mr-2"></i>
+              <a data-parent="#faq-accordion" data-toggle="collapse" class="card-toggle collapsed"
+                href="#education1" aria-expanded="false">
+                BSC Bachelor degree of science in Information technologies MEKELLE INSTITUTE OF TECHNOLOGY (MIT)
+              </a>
+              <span>2018-2024</span>
+            </h4><br>
+            <h4 class="card-title mb-0 exp">
+              <i class="fas fa-graduation-cap mr-2"></i>
+              <a data-parent="#faq-accordion" data-toggle="collapse" class="card-toggle collapsed"
+                href="#education1" aria-expanded="false">
+               preparatory school in kinfe scondary High school and preparatory school   </a>
+              <span>2016-2015</span>
+            </h4><br>
+            <h4 class="card-title mb-0 exp">
+              <i class="fas fa-graduation-cap mr-2"></i>
+              <a data-parent="#faq-accordion" data-toggle="collapse" class="card-toggle collapsed"
+                href="#education1" aria-expanded="false">
+               High school in kinfe scondary High school and preparatory school  </a>
+              <span>2014-2015</span>
+            </h4>
+            <h4 class="card-title mb-0 exp"><br/>
+              <i class="fas fa-graduation-cap mr-2"></i>
+              <a data-parent="#faq-accordion" data-toggle="collapse" class="card-toggle collapsed"
+                href="#education1" aria-expanded="false">
+               elementary school in Adiselam primary  school  </a>
+              <span>2006-2013</span>
+            </h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.mr-auto {
+  margin-right: auto !important;
+}
+</style>
+
+<script>
+export default {
+  name: 'MyExperience'
+}
+</script>
