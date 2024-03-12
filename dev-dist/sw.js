62233208c8b243009eec0d7c2fcdb86598967036
tesfish2012 <tesfay3814@email.com>
2024-03-12 02:55:02 -0800

    commit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.154cg1t67kg"
+    "revision": "0.t98tfps0b5"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
